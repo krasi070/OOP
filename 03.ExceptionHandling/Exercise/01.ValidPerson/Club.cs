b8fae7432bc9bc9e251d3564be558fd6f04dfb76
krasi070 <krasi0707@gmail.com>
2015-11-22 14:46:31 +0200

    added exceptions exercise

---
+++ 03.ExceptionHandling/Exercise/01.ValidPerson/Club.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Club
+{
+    static void Main()
+    {
+        try
+        {
+            Person rick = new Person("Rick", "Sanchez", 60);
+            Person minusAge = new Person("Howard", "Richardson", -34);
+            Person noLastName = new Person("Harry", string.Empty, 23);
+            Person nullFirstName = new Person(null, "Smith", 0);
+
+            Console.WriteLine("Name: {0} {1}\nAge: {2}", rick.FirstName, rick.LastName, rick.Age);
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("Name cannot be null value!");
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine("Age must be between 0 and 120!");
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine("Name cannot be empty space!");
+        }
+    }
+}
+
---
+++ 03.ExceptionHandling/Exercise/01.ValidPerson/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("First name cannot be null value!");
+            }
+            if (value.Trim().Length == 0)
+            {
+                throw new ArgumentException("First name cannot be empty space!");
+            }
+            this.firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Last name cannot be null value!");
+            }
+            if (value.Trim().Length == 0)
+            {
+                throw new ArgumentException("Last name cannot be empty space!");
+            }
+            this.lastName = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Age cannot be negative!");
+            }
+            if (value > 120)
+            {
+                throw new ArgumentOutOfRangeException("Age cannot be over 120!");
+            }
+            this.age = value;
+        }
+    }
+}
+
