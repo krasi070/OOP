d2f8cee98f5f0960a9e00ed3fdb0a153fa971e02
krasi070 <krasi0707@gmail.com>
2015-11-23 09:00:06 +0200

    added exception handling homework

---
+++ 03.ExceptionHandling/ExceptionHandlingHomework/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                throw new ArgumentException();
+            }
+            Console.WriteLine(Math.Sqrt(n));
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
+
---
+++ 03.ExceptionHandling/ExceptionHandlingHomework/02.EnterNumbers/EnterNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class EnterNumber
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ReadNumber(1, 100);  
+        }
+    }
+
+    private static void ReadNumber(int start, int end)
+    {
+        try
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (start > n || end < n)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine("Please enter number again");
+            ReadNumber(start, end);
+        }
+        catch (ArgumentOutOfRangeException ae)
+        {
+            Console.WriteLine("Please enter number again");
+            ReadNumber(start, end);
+        }
+    }
+}
+
